f1a1244a8caf997e83f6becb3d3074e7f704c7f9
Chris Murray <78286154+CMurray579@users.noreply.github.com>
2025-05-13 10:40:57 -0500

    c6

--- jobs/hello_world.py
+++ jobs/hello_world.py
@@ -1,12 +1,9 @@
 from nautobot.apps.jobs import Job
 
-name = "Examples"
-
 class HelloWorldJob(Job):
+
 	class Meta:
 		name = "Hello World"	#Job Name
 
     def run(self):
         self.logger.info("Hello World!") #This is the logic of the job
-        
-jobs.register_jobs(HelloWorldJob)
