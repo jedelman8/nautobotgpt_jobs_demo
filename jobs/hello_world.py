eb5171f9c2faaa11b02c42c461a8ec02d8ae1aa7
Chris Murray <78286154+CMurray579@users.noreply.github.com>
2025-05-13 10:48:20 -0500

    c8

--- jobs/hello_world.py
+++ jobs/hello_world.py
@@ -5,5 +5,5 @@ class HelloWorldJob(Job):
 	class Meta:
 		name = "Hello World"	#Job Name
 
-    def run(self):
-        self.logger.info("Hello World!") #This is the logic of the job
+	def run(self):
+		self.logger.info("Hello World!") #This is the logic of the job
