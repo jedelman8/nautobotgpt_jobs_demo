43de5e0fad33bbea8d392e18bb864cb16e120bbb
Chris Murray <78286154+CMurray579@users.noreply.github.com>
2025-05-15 15:51:12 -0500

    c

--- jobs/__init__.py
+++ jobs/__init__.py
@@ -2,7 +2,7 @@ from nautobot.apps.jobs import register_jobs
 from .hello_world import HelloWorldJob
 from .device_uptime_checker import DeviceUptimeCheck
 from .dupliacte_IP_checker import CheckDuplicateIPAddresses
-from .interface_description_search_alpha.py import InterfaceDescriptionSearch
+from .interface_description_search_alpha import InterfaceDescriptionSearch
 
 
 register_jobs(
