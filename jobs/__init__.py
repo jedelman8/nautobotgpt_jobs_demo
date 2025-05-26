bc871f2414b32a8d93d80244237f82845185b076
Chris Murray <78286154+CMurray579@users.noreply.github.com>
2025-05-26 09:13:31 +0200

    c

--- jobs/__init__.py
+++ jobs/__init__.py
@@ -1,14 +1,12 @@
 from nautobot.apps.jobs import register_jobs
 from .hello_world import HelloWorldJob
 from .device_uptime_checker import DeviceUptimeCheck
-from .dupliacte_IP_checker import CheckDuplicateIPAddresses
 from .interface_description_search_alpha import InterfaceDescriptionSearch
 from .ipv4_check import DevicesRequirePrimaryIPv4
 
 register_jobs(
 	HelloWorldJob,
 	DeviceUptimeCheck,
-	CheckDuplicateIPAddresses,
 	InterfaceDescriptionSearch,
 	DevicesRequirePrimaryIPv4
 	)
